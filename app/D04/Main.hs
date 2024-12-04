1df3e796cc2e79988596cb77b40c7f1da2ac1a5a
Bilel MEDIMEGH <bilel.medimegh@gmail.com>
2024-12-05 00:57:13 +0100

    day 04 done

--- app/D04/Main.hs
+++ app/D04/Main.hs
@@ -1,10 +1,12 @@
-module D04.Main where
+module D04.Main (main) where
 
+import Data.Functor
 import Data.List
 import Data.Maybe
+import Data.Universe.Helpers
+import GHC.Arr (Ix (range), array)
 import Text.Parsec
 import Text.Parsec.String
-import Data.Universe.Helpers
 
 xmasFile :: GenParser Char st [[XMAS]]
 xmasFile = sepEndBy xmasLine endOfLine
@@ -17,17 +19,47 @@ data XMAS = XMAS deriving (Show)
 parseXMAS :: GenParser Char st XMAS
 parseXMAS = XMAS <$ string' "XMAS"
 
-main :: IO ()
-main = do
-  base <- readFile "app/D04/input"
+step1 :: String -> IO ()
+step1 base = do
   let vert = unlines . transpose . lines
   let inv = unlines . (fmap reverse) . lines
   let diag = unlines . diagonals . lines
   let par = parse xmasFile ""
-  let appAll = \f -> \x -> [x,f x]
 
-  let a = traverse par [base, inv base, vert base, inv $ vert base, diag base, inv $ diag base,diag $ vert $ base, inv $ diag $ vert base]
-  print $ a
-  print $ length $ concat $ concat $ concat a
+  let straights = [base, inv base, vert base, inv $ vert base]
+  let diags = [diag base, inv $ diag base, diag $ inv $ base, inv $ diag $ inv $ base]
+  let out = traverse par (straights ++ diags)
+  print $ length $ concat $ concat $ concat out
 
+data PossCross = PossCross {center :: Char, tl :: Char, tr :: Char, bl :: Char, br :: Char} deriving (Show, Eq)
 
+isCrossMass :: PossCross -> Bool
+isCrossMass PossCross {center = 'A', tl = 'M', tr = 'M', bl = 'S', br = 'S'} = True
+isCrossMass PossCross {center = 'A', tl = 'S', tr = 'M', bl = 'S', br = 'M'} = True
+isCrossMass PossCross {center = 'A', tl = 'M', tr = 'S', bl = 'M', br = 'S'} = True
+isCrossMass PossCross {center = 'A', tl = 'S', tr = 'S', bl = 'M', br = 'M'} = True
+isCrossMass _ = False
+
+step2 :: String -> IO ()
+step2 base = do
+  let tableau = lines base :: [[Char]]
+  let gElem = \(x, y) -> tableau !! y !! x
+  print "Step 2"
+  let maxx = length (tableau !! 0) - 1
+  let maxy = length tableau - 1
+  let coords_list = range ((0, 0), (maxx, maxy))
+  let mapped =
+        catMaybes $
+          coords_list <&> \(x, y) ->
+            if x == 0 || y == 0 || x == maxx || y == maxy
+              then
+                Nothing
+              else
+                Just $ PossCross {center = gElem (x, y), tl = gElem (x - 1, y - 1), tr = gElem (x + 1, y - 1), bl = gElem (x - 1, y + 1), br = gElem (x + 1, y + 1)}
+  print $ length $ filter id $ isCrossMass <$> mapped
+
+main :: IO ()
+main = do
+  base <- readFile "app/D04/input"
+  step1 base
+  step2 base
